718d5aff0fd91e9551ed8f260fcd3a94927b906a
Gary Katsevman <git@gkatsev.com>
2017-05-17 15:29:56 -0400

    we no longer need alias

--- src-es6/rollup.js
+++ src-es6/rollup.js
@@ -1,16 +1,12 @@
 import babel from 'rollup-plugin-babel';
 import cjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
-import alias from 'rollup-plugin-alias';
 import path from 'path';
 
 export default {
   entry: 'src-es6/index.js',
   format: 'iife',
   plugins: [
-    alias({
-      'videojs-vtt.js': path.resolve(__dirname, '../node_modules/videojs-vtt.js/dist/vtt.js')
-    }),
     resolve({
       jsnext: true,
       main: true,
